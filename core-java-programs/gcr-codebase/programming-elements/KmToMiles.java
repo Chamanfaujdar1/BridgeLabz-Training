cc40965b9f41d19534254fc11a4255d43f47959a
Chamanfaujdar1 <132120396+Chamanfaujdar1@users.noreply.github.com>
2025-12-17 20:48:06 +0530

    [Chaman Faujdar] 8,9,10 questions committed

---
+++ core-java-programs/gcr-codebase/programming-elements/KmToMiles.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class KiloToMiles{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		double Kilometers = sc.nextDouble();
+		double Miles = Kilometers * 0.621371;
+		System.out.println(Miles);
+	}
+}
---
+++ core-java-programs/gcr-codebase/programming-elements/PowerCalculation.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Power{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int base = sc.nextInt();
+		int exponent = sc.nextInt();
+
+		System.out.println(Math.pow(base, exponent));
+	}
+}
